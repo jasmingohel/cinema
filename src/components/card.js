63aa1b9be66938db32d81ff167f246e3ba220c0d
Jasmin Gohel <jasmin.gohel@krishtechnolabs.com>
2024-08-14 12:00:11 +0530

    checking update

--- src/components/card.js
+++ src/components/card.js
@@ -12,7 +12,7 @@ function Card() {
       <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
       <h3 class="z-10 mt-3 text-3xl font-bold text-white">Paris</h3>
       <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-        City of love
+        City of <h1>Sex </h1>
       </div>
     </article>
   );
